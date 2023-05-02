a1474e624322822010e9188d168397f9450f5d9d
Pablo <pablito@me.com>
2023-05-02 19:36:41 +0100

    TDD button for new note input

--- noteView.js
+++ noteView.js
@@ -2,11 +2,34 @@
 
 class NoteView {
   constructor(model) {
+    this.buttonEl = document.querySelector('#new-note-button');
     this.model = model;
     this.mainContainerEl = document.querySelector('.main_container');
+
+    this.buttonEl.addEventListener('click', () => {
+      let newNote = document.querySelector('#new-note-input').value;
+      this.addNewNote(newNote);
+    });
+  }
+
+  clearInputField() {
+    document.querySelector('#new-note-input').value = ""
+  }
+
+  addNewNote(newNote) {
+    this.model.addNote(newNote);
+    this.clearInputField();
+    this.displayNotes();
+  }
+
+  clearNotes() {
+    const allNotes = document.querySelectorAll('.note');
+    allNotes.forEach(note => note.remove());
   }
 
   displayNotes() {
+    this.clearNotes();
+
     const notes = this.model.getNotes();
     notes.forEach(note => {
       const newDivEl = document.createElement('div');
@@ -16,7 +39,6 @@ class NoteView {
       newDivEl.append(newParaEl);
       const mainDivEl = document.querySelector('.main_container');
       mainDivEl.append(newDivEl);
-      // this.mainContainerEl.append(newDivEl);
     })
   }
 }
--- noteView.test.js
+++ noteView.test.js
@@ -10,36 +10,48 @@ let view;
 
 describe('TodoListView', () => {
   beforeEach(() => {
+    document.body.innerHTML = fs.readFileSync('./index.html');
     model = new Model();
     view = new View(model);
   });
 
   test('should display a blank list of notes when there are no notes', () => {
-    document.body.innerHTML = fs.readFileSync('./index.html');
     expect(document.querySelectorAll('div').length).toBe(1);
     expect(document.querySelectorAll('div.note').length).toBe(0);
     expect(document.querySelectorAll('p').length).toBe(0);
   });
 
   test('should display a list of notes when notes are added', () => {
-    document.body.innerHTML = fs.readFileSync('./index.html');
     model.addNote('Cancel subscription to Netflix');
     model.addNote('Go on holiday');
     view.displayNotes();
+    
     expect(document.querySelectorAll('div').length).toBe(3);
     expect(document.querySelectorAll('div.note').length).toBe(2);
     expect(document.querySelectorAll('p').length).toBe(2);
   });
   
   test('should display a blank list of notes when there are no notes', () => {
-    document.body.innerHTML = fs.readFileSync('./index.html');
     model.addNote('Cancel subscription to Netflix');
     model.addNote('Go on holiday');
     model.addNote('Get more sleep');
     model.addNote('Eat cake');
     view.displayNotes();
+
     expect(document.querySelectorAll('div').length).toBe(5);
     expect(document.querySelectorAll('div.note').length).toBe(4);
     expect(document.querySelectorAll('p').length).toBe(4);
+  });
+  
+  test('should create a new note when the button is clicked', () => {
+    const inputEl = document.querySelector('#new-note-input');
+    inputEl.value = "This is a new note test";
+    
+    const buttonEl = document.querySelector('#new-note-button');
+    buttonEl.click();
+
+    expect(document.querySelectorAll('div').length).toBe(2);
+    expect(document.querySelectorAll('div.note').length).toBe(1);
+    expect(document.querySelectorAll('p')[0].textContent).toEqual("This is a new note test");
   });
 });
